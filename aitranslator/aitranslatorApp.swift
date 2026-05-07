f57b38b2f7a77995cf6e82bc1e6073f1ffca8079
Alec <alec.lee1005@gmail.com>
2026-05-07 09:45:54 +0900

    Initial Commit

---
+++ aitranslator/aitranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  aitranslatorApp.swift
+//  aitranslator
+//
+//  Created by Alec Lee on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct aitranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
